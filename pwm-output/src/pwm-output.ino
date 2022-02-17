c5394f4b6e0eacf02289bf9f8dc24c2444fd75e7
CedrikJiruse <github.meow@gmail.com>
2022-02-17 16:18:13 +0000

    add pathetic attempt at differential steering

--- pwm-output/src/pwm-output.ino
+++ pwm-output/src/pwm-output.ino
@@ -2,13 +2,19 @@
 
 #define throttlePin 26
 #define steeringPin 13
-#define motorPin 23
+#define motor1Pin 23
+// #define motor2Pin 22
 #define speedPin 12
 #define brakePin 33
 #define motorLowPin 35
 #define motorHighPin 34
 
-int pwmChannel = 0;
+//Left & right diff
+int LeftPWM;
+// int RightPWM;
+
+int leftMotorChannel = 1;
+// int rightMotorChannel = 2;
 int pwmMin = 0, pwmMax = 4095;
 volatile int microSpeed = 0;
 volatile int prev_time = 0;
@@ -23,55 +29,63 @@ void setup() {
   pinMode(motorHighPin, INPUT);
   pinMode(steeringPin, INPUT);
 
-  pinMode(motorPin, OUTPUT);
+  pinMode(motor1Pin, OUTPUT);
+  // pinMode(motor2Pin, OUTPUT);
 
-  ledcSetup(pwmChannel, 50, 12);
-  ledcAttachPin(motorPin, pwmChannel);
+  ledcSetup(leftMotorChannel, 50, 12);
+  ledcAttachPin(motor1Pin, leftMotorChannel);
+  // ledcSetup(rightMotorChannel, 50, 12);
+  // ledcAttachPin(motor2Pin, rightMotorChannel);
 
   attachInterrupt(speedPin, rising, RISING);
 }
 
 void loop() {
   // Motor
-  int dutyCycle;
+  int motor1Duty;
+  // int motor2Duty;
   int motorHigh = digitalRead(motorHighPin);
   int motorLow = digitalRead(motorLowPin);
   int brake = digitalRead(brakePin);
 
   // set the duty cycle based on throttle position and sensors
   if (motorHigh == HIGH && motorLow == LOW && brake == LOW) {
-    dutyCycle = pwmMax;
+    motor1Duty = pwmMax;
   }
   if (brake == LOW && motorLow == LOW && motorHigh == LOW) {
-    dutyCycle = map(analogRead(throttlePin), 0, 4095, pwmMin, pwmMax);
+    motor1Duty = map(analogRead(throttlePin), 0, 4095, pwmMin, pwmMax);
   }
   if (motorLow == HIGH || brake == HIGH) { // change BRAKE from high to low when connecting to car
-    dutyCycle = 0;                         // this is done so that we dont have to press the brake every time we want to test duty cycle
+    motor1Duty = 0;                        // this is done so that we dont have to press the brake every time we want to test duty cycle
   }
 
-  // Steering
-  int steering = analogRead(steeringPin);
+  // // Steering
+  int steering = map(analogRead(steeringPin), 0, 4095, -2048, 2047);
 
-  // Differential
+  // // Differential
+  // RightPWM = steering;
+
+  // LeftPWM = (steering / 2048) * motor1Duty;
 
 
-  ledcWrite(pwmChannel, dutyCycle);
+  ledcWrite(leftMotorChannel, motor1Duty);
+  // ledcWrite(rightMotorChannel, RightPWM);
 
   Serial.print("Motor Low: ");
   Serial.print(motorLow);
-  Serial.print("\t");
-  Serial.print("Motor High: ");
+  Serial.print(" Motor High: ");
   Serial.print(motorHigh);
-  Serial.print("\t");
-  Serial.print("Brake: ");
+  Serial.print(" Brake: ");
   Serial.print(brake);
+  Serial.print(" Steering");
+  Serial.print(steering);
   Serial.print("\t");
-  Serial.print("Steering");
-  // Serial.print(steering);
-  Serial.print("\t");
-  Serial.print("Duty Cycle: ");
-  Serial.print(dutyCycle);
-  Serial.print("       ");
+  // Serial.print("Left Duty Cycle: ");
+  // Serial.print(LeftPWM);
+  // Serial.print("       ");
+  // Serial.print("Right Duty Cycle: ");
+  // Serial.print(RightPWM);
+  // Serial.print("       ");
   Serial.print("Speed: ");
   Serial.print(microSpeed);
   Serial.print("\n");
